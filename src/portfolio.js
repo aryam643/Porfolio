dbbcf8e7ca4131a9d3268026a58b10be42daab2d
Aryam Sharma <93140448+aryam643@users.noreply.github.com>
2024-01-20 23:52:37 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,7 @@ const about = {
   name: 'Aryam',
   role: 'Front End Developer and ML Enthusiast',
   description:
-    'I am currently a pre-final year student at Jaypee University of Information Technology and pursuing my B.Tech from here. I am a self taught Front End Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.',
+    'I am currently a pre-final year student at Jaypee University of Information Technology, pursuing my B.Tech here. I am a self-taught Front-End Developer, currently delving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive about their dreams and keep working towards them.',
   resume: 'https://purple-bird-98.tiiny.site',
   social: {
     linkedin: 'https://www.linkedin.com/in/aryamsharma/',
