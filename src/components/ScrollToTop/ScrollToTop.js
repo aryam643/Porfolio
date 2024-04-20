a0d1ca60f1f424528f6d909f4f74244a7065ce4e
Aryam <sarthi.aryam.sharma@gmail.com>
2024-04-21 01:12:14 +0530

    update1

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
-import './ScrollToTop.css'
+import React, { useEffect, useState } from 'react';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import './ScrollToTop.css';
 
 const ScrollToTop = () => {
-  const [isVisible, setIsVisible] = useState(false)
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const toggleVisibility = () =>
-      window.pageYOffset > 500 ? setIsVisible(true) : setIsVisible(false)
+      window.pageYOffset > 500 ? setIsVisible(true) : setIsVisible(false);
 
-    window.addEventListener('scroll', toggleVisibility)
-    return () => window.removeEventListener('scroll', toggleVisibility)
-  }, [])
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
 
   return isVisible ? (
     <div className='scroll-top'>
-      <a href='#top'>
+<button type="button" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
         <ArrowUpwardIcon fontSize='large' />
-      </a>
+        <span className="sr-only">Scroll to top</span>
+      </button>
     </div>
-  ) : null
-}
+  ) : null;
+};
 
-export default ScrollToTop
+export default ScrollToTop;
